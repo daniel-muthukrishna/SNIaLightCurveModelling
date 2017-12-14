19647e946b9e774c8ea02b27a4b9709bada45520
danmuth <daniel.muthukrishna@gmail.com>
2017-12-14 15:32:07 +0000

    Created Class for PopulationStatistics

--- read_data.py
+++ read_data.py
@@ -79,36 +79,23 @@ class LightCurve(object):
         return peakPhases, peakFluxes
 
 
-def get_filenames(band):
-    directory = os.path.join('NIR_Lowz_data', band)
-    filenameList = os.listdir(directory)
-    filePathList = [os.path.join(directory, f) for f in filenameList]
+class PopulationStatistics(object):
+    def __init__(self, filenameList, bandName):
+        self.filenameList = filenameList
+        self.bandName = bandName
 
-    return filePathList
+    def plot_binned_light_curves(self, colorMarker):
+        xBinsList, yBinsList, peaks = [], [], []
+        zorder = 200
 
-
-def main():
-    bandList = ['H_band', 'J_Band', 'K_band', 'Y_Band']
-    colors = ['#ff7f0e', '#2ca02c', '#d62728', '#1f77b4', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
-              '#17becf', 'k', '#911eb4', '#800000', '#aa6e28']
-    markers = ['o', 'v', 'P', '*', 'D', 'X', 'p', '3', 's', 'x', 'p']
-    colorMarker = []
-    for color in colors:
-        for marker in markers:
-            colorMarker.append((color, marker))
-
-    for band in bandList:
-        xBinsList, yBinsList = [], []
         fig, ax = plt.subplots(2, sharex=True)
-        ax[0].set_title(band)
+        ax[0].set_title(self.bandName)
         ax[0].set_ylabel('Abs mag')
         ax[0].invert_yaxis()
         ax[1].invert_yaxis()
         ax[1].set_ylabel('Maxima')
-        zorder = 200
 
-        filenameList = get_filenames(band)
-        for i, filename in enumerate(filenameList):
+        for i, filename in enumerate(self.filenameList):
             zorder -= 1
             lightCurve = LightCurve(filename)
             lightCurve.plot_light_curves(axis=ax[0], cm=colorMarker[i], zorder=zorder)
@@ -117,8 +104,10 @@ def main():
                 xBinsList.append(xBins)
                 yBinsList.append(yBins)
                 peakPhases, peakFluxes = lightCurve.get_peaks(axis=ax[1], cm=colorMarker[i], zorder=zorder)
+                peaks.append([peakPhases, peakFluxes])
             except TypeError:
                 pass
+
         xBinsArray, yBinsArray = np.array(xBinsList), np.array(yBinsList)
         averageLC = np.nanmean(yBinsArray, axis=0)
         errorsLC = np.nanstd(yBinsArray, axis=0)
@@ -129,9 +118,40 @@ def main():
 
         plt.xlabel('Phase (days)')
         plt.xlim(-20, 100)
-        plt.savefig(band)
-
         # plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., ncol=1)
+        plt.savefig(self.bandName)
+
+        return xBinsArray, yBinsArray, np.array(peaks)
+
+
+def get_filenames(band):
+    directory = os.path.join('NIR_Lowz_data', band)
+    filenameList = os.listdir(directory)
+    filePathList = [os.path.join(directory, f) for f in filenameList]
+
+    return filePathList
+
+
+def get_colors_and_markers():
+    colors = ['#ff7f0e', '#2ca02c', '#d62728', '#1f77b4', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
+              '#17becf', 'k', '#911eb4', '#800000', '#aa6e28']
+    markers = ['o', 'v', 'P', '*', 'D', 'X', 'p', '3', 's', 'x', 'p']
+    colorMarker = []
+    for color in colors:
+        for marker in markers:
+            colorMarker.append((color, marker))
+    return colorMarker
+
+
+def main():
+    bandList = ['H_band', 'J_Band', 'K_band', 'Y_Band']
+    colorMarker = get_colors_and_markers()
+
+    for band in bandList:
+        filenameList = get_filenames(band)
+        popStats = PopulationStatistics(filenameList, band)
+        xBinsArray, yBinsArray, peaks = popStats.plot_binned_light_curves(colorMarker)
+
     plt.show()
 
 
